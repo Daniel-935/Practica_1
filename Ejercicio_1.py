03e355ea92e6058821d69c82ee09d0e1d30ce307
Daniel-935 <daniel.bustos4669@alumnos.udg.mx>
2023-09-06 20:01:54 -0600

    El programa realiza el entrenamiento y el algoritmo de pruebas, mostrando de forma grafica los resultados obtenidos
    Se agregan tres imagenes que muestran los distintos resultados en tres corridas del programa. En la funcion de test ya no se realizan los ajustes de peso, pues solo utiliza los correspondientes del entrenamiento.
    Los puntos rojos en la grafica son los puntos mal clasificados y los verdes los bien clasificados.

--- Ejercicio_1.py
+++ Ejercicio_1.py
@@ -20,8 +20,21 @@ myPerceptron.setBias(np.random.rand())
 myPerceptron.setW0(np.random.rand())
 myPerceptron.setW1(np.random.rand())
 #* Inicia el perceptron
-myPerceptron.startPerceptron(trainingSet)
+myPerceptron.trainPerceptron(trainingSet)
 
 #*Una vez terminado, muestra los pesos finales y el bias
 
 print(f"Pesos finales. w0 = {myPerceptron.getW0()} w1 = {myPerceptron.getW1()} y Bias = {myPerceptron.getBias()}")
+
+#* Prueba del perceptron
+testSet = []
+with open('XOR_tst.csv','r') as trainFile:
+
+    csvReader = csv.reader(trainFile)
+
+    for row in csvReader:
+        testSet.append(row)
+
+myPerceptron.testPerceptron(testSet)
+
+print(f"Pesos finales despues del test. w0 = {myPerceptron.getW0()} w1 = {myPerceptron.getW1()} y Bias = {myPerceptron.getBias()}")
--- Perceptron.py
+++ Perceptron.py
@@ -1,4 +1,5 @@
 import numpy as np
+from matplotlib import pyplot
 
 class Perceptron:
 
@@ -10,6 +11,12 @@ class Perceptron:
         self.x0 = 0.0
         self.x1 = 0.0
         self.y = 0.0
+
+        #* Listas creadas para guardar los puntos correspondientes del test
+        self.puntosX = []
+        self.puntosY = []
+        #* Lista para clasificar los puntos por colores y graficarlos
+        self.clasifColores = []
 
     #*Funcion de activacion
     def funcActivacion(self, out):
@@ -29,7 +36,7 @@ class Perceptron:
 
     #*Funcion que servirá tanto para entrenamiento como para los datos reales
     #!data = datos del csv
-    def startPerceptron(self, data, factorAprendizaje=0.4,maxEpocas=100):
+    def trainPerceptron(self, data, factorAprendizaje=0.4,maxEpocas=100):
 
         row = []
         error = 0
@@ -56,6 +63,46 @@ class Perceptron:
                     self.ajustaPesos(factorAprendizaje, error)
                     break
 
+    def testPerceptron(self, data, factorAprendizaje=0.4):
+
+        row = []
+        error = 0
+
+        for i in range(len(data)):
+
+            #* Coloca las entradas
+            row = data[i]
+            self.setX0(float(row[0]))
+            self.setX1(float(row[1]))
+            self.setY(float(row[2]))
+            #*Obtiene el valor Y
+            sum = (self.sumatoria())
+
+            #* Guarda en el array de colores la clasificacion que realiza el perceptron
+            #! Si es igual a la salida deseada, se guarda verde, de lo contrario es rojo
+            if self.funcActivacion(sum) == self.getY():
+                self.clasifColores.append('green')
+            else:
+                self.clasifColores.append('red')
+            #*Calcula el error (valor deseado - Y)
+            error = self.getY() - (self.funcActivacion(sum))
+
+            #*Como es el metodo para testear, no se hace el ajuste de pesos
+
+        #*Al final se grafican los puntos (c= colores de cada punto)
+        self.crearPuntos(data)
+
+        pyplot.scatter(self.puntosX, self.puntosY, c=self.clasifColores)
+        pyplot.show()
+        #print(f"Long X: {len(self.puntosX)} Long Y: {len(self.puntosY)} Long Color: {len(self.clasifColores)}")
+
+    #*Metodo para crear los puntos de las coordenadas
+    def crearPuntos(self, data):
+
+        for i in range(len(data)):
+
+            self.puntosX.append(float(data[i][0]))
+            self.puntosY.append(float(data[i][1]))
 
     #* Getters y setters
     def setW0(self,w):
